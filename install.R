4724ea40e5f72fe3ceb6986bd33cb1e701eefe6f
Joong-Ho (Johann) Won <wonj@stats.snu.ac.kr>
2026-04-07 21:58:23 +0900

    first commit

---
+++ install.R
@@ -0,0 +1,6 @@
+# 1. 기초 패키지 및 CRAN 패키지 설치
+install.packages("Matrix")
+install.packages("Rlinsolve")  # iterative linear solvers
+install.packages("SparseM") # for visualization
+install.packages("lobstr")
+install.packages("microbenchmark")
